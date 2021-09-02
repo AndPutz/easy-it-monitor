32da949b527e7f5eed25981c261147a44e51c7b2
AndPutz <and.putz@gmail.com>
2021-09-02 13:23:40 -0300

    feat: Service and Process Description updated

--- Infra/SystemInfo.cs
+++ Infra/SystemInfo.cs
@@ -11,10 +11,12 @@ namespace Infra
     public static class SystemInfo
     {
         public const string AGENT_MONITOR_NAME = "EasyITMonitorAgent";
-        public const string AGENT_MONITOR_DESCRIPTION = "Easy IT Monitor";
+        public const string AGENT_MONITOR_DISPLAY = "Easy IT Monitor Agent";
+        public const string AGENT_MONITOR_DESCRIPTION = "Real-time monitoring of IT components and services, such as networks, servers and applications";
 
         public const string AGENT_WATCHDOG_NAME = "EasyITWatchDogAgent";
-        public const string AGENT_WATCHDOG_DESCRIPTION = "Easy IT WatchDog";
+        public const string AGENT_WATCHDOG_DISPLAY = "Easy IT WatchDog Agent";
+        public const string AGENT_WATCHDOG_DESCRIPTION = "IT Automation to recovery services and applications";
 
         public static int KeepAliveTimer { get; set; } = Convert.ToInt32(ConfigurationManager.AppSettings["KeepAliveTimer"]);
         public static int CycleTimer { get; set; } = Convert.ToInt32(ConfigurationManager.AppSettings["CycleTimer"]);
